cff020ead058b355837646c92c001b3a4e37b2b1
Saddam-tech <salokhiddinov0727@gmail.com>
2023-03-15 12:40:19 +0900

    added Mumbai and Goerli testnet credentials in the configs file

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 require("./tasks/get-accounts");
 /** @type import('hardhat/config').HardhatUserConfig */
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   defaultNetwork: "hardhat",
@@ -11,6 +12,11 @@ module.exports = {
       url: GOERLI_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 5,
+    },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
     },
   },
   solidity: "0.8.0",
