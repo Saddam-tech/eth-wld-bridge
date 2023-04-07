0aa9ae21c6fb7a986c5046273b4108d6a157b642
Saddam-tech <salokhiddinov0727@gmail.com>
2023-04-07 19:27:13 +0900

    bug fixes

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,27 +2,12 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/get-accounts");
 /** @type import('hardhat/config').HardhatUserConfig */
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    worldland: {
-      url: "https://rpc.lvscan.io",
-      accounts: [
-        "36ce51e7722a9dadedf6dedc8210f4949db2f7aa031d2d10190e8ea5312189d9",
-      ],
-      chainId: 12345,
-      gas: 100000,
-    },
-    goerli: {
-      url: GOERLI_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 5,
-      gas: 100000,
-    },
     sepolia: {
       url: SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
@@ -33,6 +18,14 @@ module.exports = {
       url: MUMBAI_RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 80001,
+      gas: 100000,
+    },
+    worldland: {
+      url: "https://rpc.lvscan.io",
+      accounts: [
+        "36ce51e7722a9dadedf6dedc8210f4949db2f7aa031d2d10190e8ea5312189d9",
+      ],
+      chainId: 12345,
       gas: 100000,
     },
     localhost_1: {
