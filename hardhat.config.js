f7bac8dc10a18ed56c93d6c7d5b75d61c0e4732a
Saddam-tech <salokhiddinov0727@gmail.com>
2023-04-14 17:57:44 +0900

    private key encryption done

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,44 +2,44 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/get-accounts");
 /** @type import('hardhat/config').HardhatUserConfig */
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+// const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+// const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 11155111,
-      gas: 100000,
-    },
-    mumbai: {
-      url: MUMBAI_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 80001,
-      gas: 100000,
-    },
-    worldland: {
-      url: "https://rpc.lvscan.io",
-      accounts: [
-        "36ce51e7722a9dadedf6dedc8210f4949db2f7aa031d2d10190e8ea5312189d9",
-      ],
-      chainId: 12345,
-      gas: 100000,
-    },
-    localhost_1: {
-      url: process.env.provider_chain_1,
-      accounts: [PRIVATE_KEY],
-      chainId: 31337,
-      gas: 100000,
-    },
-    localhost_2: {
-      url: process.env.provider_chain_2,
-      accounts: [PRIVATE_KEY],
-      chainId: 31337,
-      gas: 100000,
-    },
+    // current supported networks
+    // sepolia: {
+    //   url: process.env.provider_chain_1,
+    //   accounts: [encryptedPk],
+    //   chainId: 11155111,
+    //   gas: 100000,
+    // },
+    // worldland: {
+    //   url: process.env.provider_chain_2,
+    //   accounts: [encryptedPk],
+    //   chainId: 12345,
+    //   gas: 100000,
+    // },
+    // // current supported networks
+    // mumbai: {
+    //   url: MUMBAI_RPC_URL,
+    //   accounts: [encryptedPk],
+    //   chainId: 80001,
+    //   gas: 100000,
+    // },
+    // localhost_1: {
+    //   url: process.env.provider_chain_1,
+    //   accounts: [encryptedPk],
+    //   chainId: 31337,
+    //   gas: 100000,
+    // },
+    // localhost_2: {
+    //   url: process.env.provider_chain_2,
+    //   accounts: [encryptedPk],
+    //   chainId: 31337,
+    //   gas: 100000,
+    // },
   },
   solidity: "0.8.0",
 };
