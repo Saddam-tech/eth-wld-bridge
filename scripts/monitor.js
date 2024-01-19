a8e2bba09fcc9706ea1e628594d24b03e6c4c3cb
Saddam-tech <salokhiddinov0727@gmail.com>
2024-01-19 16:41:45 +0900

    optimzed the code

--- scripts/monitor.js
+++ scripts/monitor.js
@@ -55,17 +55,8 @@ const WALLET_CHAIN_2 = new ethers.Wallet(encryptedPk, CHAIN_2_PROVIDER);
 
 async function processTransactionQueue() {
   try {
-    // ETH => WETH
-    let mintWETHTxQueueChain1 = [];
-    let mintWETHTxQueueChain2 = [];
-    let unLockETHTxQueueChain1 = [];
-    let unLockETHTxQueueChain2 = [];
-
-    // Token => Token
-    let mintTokenTxQueueChain1 = [];
-    let mintTokenTxQueueChain2 = [];
-    let unlockTokenTxQueueChain1 = [];
-    let unlockTokenTxQueueChain2 = [];
+    let queue_chain_1 = [];
+    let queue_chain_2 = [];
 
     const tx_queue = await query_all(TABLES.TX_QUEUE);
     console.log({ tx_queue });
@@ -75,165 +66,37 @@ async function processTransactionQueue() {
           // processed transactions will not exist in tx_queue table, but it does not hurt to make an extra check here
           // continue if the transaction is not processed yet
           // start sorting the transactions
-
-          // saving into MINTWETH temporary memory
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_1 &&
-            tx_queue[i].function_type === FUNCTIONS.MINTWETH
-          ) {
-            // save into chain1 MINTWETH temporary memory
-            mintWETHTxQueueChain1.push(tx_queue[i]);
+          if (tx_queue[i].chain === CHAINS.CHAIN_1) {
+            // saving into chain_1 queue temporary memory
+            queue_chain_1.push(tx_queue[i]);
           }
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_2 &&
-            tx_queue[i].function_type === FUNCTIONS.MINTWETH
-          ) {
-            // save into chain2 MINTWETH temporary memory
-            mintWETHTxQueueChain2.push(tx_queue[i]);
-          }
-
-          // save into UNLOCKETH temporary memory
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_1 &&
-            tx_queue[i].function_type === FUNCTIONS.UNLOCKETH
-          ) {
-            // save into chain1 UNLOCKETH temporary memory
-            unLockETHTxQueueChain1.push(tx_queue[i]);
-          }
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_2 &&
-            tx_queue[i].function_type === FUNCTIONS.UNLOCKETH
-          ) {
-            // save into chain2 UNLOCKETH temporary memory
-            unLockETHTxQueueChain2.push(tx_queue[i]);
-          }
-
-          // save into MINTTOKEN temporary memory
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_1 &&
-            tx_queue[i].function_type === FUNCTIONS.MINTTOKEN
-          ) {
-            // save into chain1 MINTTOKEN temporary memory
-            mintTokenTxQueueChain1.push(tx_queue[i]);
-          }
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_2 &&
-            tx_queue[i].function_type === FUNCTIONS.MINTTOKEN
-          ) {
-            // save into chain2 MINTTOKEN temporary memory
-            mintTokenTxQueueChain2.push(tx_queue[i]);
-          }
-
-          // save into UNLOCKTOKEN temporary memory
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_1 &&
-            tx_queue[i].function_type === FUNCTIONS.UNLOCKTOKEN
-          ) {
-            // save into chain1 UNLOCKTOKEN temporary memory
-            unlockTokenTxQueueChain1.push(tx_queue[i]);
-          }
-          if (
-            tx_queue[i].chain === CHAINS.CHAIN_2 &&
-            tx_queue[i].function_type === FUNCTIONS.UNLOCKTOKEN
-          ) {
-            // save into chain2 UNLOCKTOKEN temporary memory
-            unlockTokenTxQueueChain2.push(tx_queue[i]);
+          if (tx_queue[i].chain === CHAINS.CHAIN_2) {
+            // saving into chain_2 queue temporary memory
+            queue_chain_2.push(tx_queue[i]);
           }
         }
       }
     }
 
-    // chain1 mintWETH batch submission
-    if (mintWETHTxQueueChain1.length > 0) {
+    // chain 1 queue batch submission
+    if (queue_chain_1.length > 0) {
       consumeTx({
-        queue: mintWETHTxQueueChain1,
+        queue: queue_chain_1,
         contract: CHAIN_1_CONTRACT,
         wallet: WALLET_CHAIN_1,
-        method: mintWETHTxQueueChain1[0].function_type,
+        method: queue_chain_1[0].function_type,
       });
     } else {
       console.log(MESSAGES.NO_TX(1));
     }
 
-    // chain2 mintWETH batch submission
-    if (mintWETHTxQueueChain2.length > 0) {
+    // chain2 queue batch submission
+    if (queue_chain_2.length > 0) {
       consumeTx({
-        queue: mintWETHTxQueueChain2,
+        queue: queue_chain_2,
         contract: CHAIN_2_CONTRACT,
         wallet: WALLET_CHAIN_2,
-        method: mintWETHTxQueueChain2[0].function_type,
-      });
-    } else {
-      console.log(MESSAGES.NO_TX(2));
-    }
-
-    // chain1 unLockETH batch submission
-    if (unLockETHTxQueueChain1.length > 0) {
-      consumeTx({
-        queue: unLockETHTxQueueChain1,
-        contract: CHAIN_1_CONTRACT,
-        wallet: WALLET_CHAIN_1,
-        method: unLockETHTxQueueChain1[0].function_type,
-      });
-    } else {
-      console.log(MESSAGES.NO_TX(1));
-    }
-
-    // chain2 unLockETH batch submission
-    if (unLockETHTxQueueChain2.length > 0) {
-      consumeTx({
-        queue: unLockETHTxQueueChain2,
-        contract: CHAIN_2_CONTRACT,
-        wallet: WALLET_CHAIN_2,
-        method: unLockETHTxQueueChain2[0].function_type,
-      });
-    } else {
-      console.log(MESSAGES.NO_TX(2));
-    }
-
-    // chain1 mintToken batch submission
-    if (mintTokenTxQueueChain1.length > 0) {
-      consumeTx({
-        queue: mintTokenTxQueueChain1,
-        contract: CHAIN_1_CONTRACT,
-        wallet: WALLET_CHAIN_1,
-        method: mintTokenTxQueueChain1[0].function_type,
-      });
-    } else {
-      console.log(MESSAGES.NO_TX(1));
-    }
-
-    // chain2 mintToken batch submission
-    if (mintTokenTxQueueChain2.length > 0) {
-      consumeTx({
-        queue: mintTokenTxQueueChain2,
-        contract: CHAIN_2_CONTRACT,
-        wallet: WALLET_CHAIN_2,
-        method: mintTokenTxQueueChain2[0].function_type,
-      });
-    } else {
-      console.log(MESSAGES.NO_TX(2));
-    }
-
-    // chain1 unlockToken batch submission
-    if (unlockTokenTxQueueChain1.length > 0) {
-      consumeTx({
-        queue: unlockTokenTxQueueChain1,
-        contract: CHAIN_1_CONTRACT,
-        wallet: WALLET_CHAIN_1,
-        method: unlockTokenTxQueueChain1[0].function_type,
-      });
-    } else {
-      console.log(MESSAGES.NO_TX(1));
-    }
-
-    // chain2 unlockToken batch submission
-    if (unlockTokenTxQueueChain2.length > 0) {
-      consumeTx({
-        queue: unlockTokenTxQueueChain2,
-        contract: CHAIN_2_CONTRACT,
-        wallet: WALLET_CHAIN_2,
-        method: unlockTokenTxQueueChain2[0].function_type,
+        method: queue_chain_2[0].function_type,
       });
     } else {
       console.log(MESSAGES.NO_TX(2));
